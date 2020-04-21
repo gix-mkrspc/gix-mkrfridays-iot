a087bc2e387bd3169340f9f53b455bf9f4425d86
codes <hello@cody.codes>
2020-04-21 12:40:34 -0700

    update no dashboard & instructions

--- standalone/install.sh
+++ standalone/install.sh
@@ -1,7 +1,26 @@
-echo "Ensure you have Python3 installed on your machine and it's located in your \$PATH variable"
+echo "Ensure you have Python3 installed on your machine and it's
+ located in your \$PATH variable. Now installing..."
+sleep 5
 
 # create a venv in the directory you run this script from
 mkdir ./esphome
 python3 -m venv ./esphome
 source ./esphome/bin/activate
 pip install esphome tornado esptool
+
+echo "
+
+
+
+
+
+
+ ******** Install Complete! ********
+ 
+ please create a file called secrets.yaml inside the same directory
+ this script is run in! Anywhere it says !secret you need to create a key/value
+ pair where the key is what comes after secret in servo.yaml and the value is
+ the private information. Here's an example entry inside secrets.yaml:
+
+ ap_ssid: \"super secret ap ssid\"
+ "
--- standalone/run.sh
+++ standalone/run.sh
@@ -1,6 +1,3 @@
 source ./esphome/bin/activate
-# start server on default port
-esphome config/ dashboard &
 sleep 5
-# open web dashboard to default port
-open http://localhost:6052
+esphome servo.yaml run
