7c99263b896d3bf9de9b8ddf264e35b38d508769
codes <hello@cody.codes>
2020-05-18 11:33:20 -0700

    :construction: create rg

---
+++ azure_iot_hub/python-sdk/provision_rg.py
@@ -0,0 +1,37 @@
+# Import the needed management objects from the libraries. The azure.common library
+# is installed automatically with the other libraries.
+from azure.common.client_factory import get_client_from_cli_profile
+from azure.mgmt.resource import ResourceManagementClient
+
+RESOURCE_GROUP_NAME = "iot-porg"
+RESOURCE_GROUP_LOCATION = "centralus"
+
+# Obtain the management object for resources, using the credentials from the CLI login.
+resource_client = get_client_from_cli_profile(ResourceManagementClient)
+
+# Provision the resource group.
+rg_result = resource_client.resource_groups.create_or_update(
+    f"{RESOURCE_GROUP_NAME}",
+    {
+        "location": f"{RESOURCE_GROUP_LOCATION}"
+    }
+)
+
+# Within the ResourceManagementClient is an object named resource_groups,
+# which is of class ResourceGroupsOperations, which contains methods like
+# create_or_update.
+#
+# The second parameter to create_or_update here is technically a ResourceGroup
+# object. You can create the object directly using ResourceGroup(location=LOCATION)
+# or you can express the object as inline JSON as shown here. For details,
+# see Inline JSON pattern for object arguments at
+# https://docs.microsoft.com/azure/developer/python/azure-sdk-overview#inline-json-pattern-for-object-arguments.
+
+print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} region")
+
+# The return value is another ResourceGroup object with all the details of the
+# new group. In this case the call is synchronous: the resource group has been
+# provisioned by the time the call returns.
+
+# Optional line to delete the resource group
+#resource_client.resource_groups.delete("PythonSDKExample-ResourceGroup-rg")
