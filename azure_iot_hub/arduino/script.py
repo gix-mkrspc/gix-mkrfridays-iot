3994b43c23bff492dde8c5409032b8ac4e7e2713
codes <hello@cody.codes>
2020-05-18 17:40:58 -0700

    :pencil2: docstring + implementation comment

--- azure_iot_hub/arduino/script.py
+++ azure_iot_hub/arduino/script.py
@@ -16,7 +16,7 @@ def update_line_file(file_path, str_line_to_update, str_replacement, comment_onl
     :type file_path: str
     :param str_to_update: The string which will be replaced
     :type str_to_update str:
-    :param str_replacement: The string which will be replaced
+    :param str_replacement: The string which replace the line of str_to_update
     :type str_replacement str:
     :param comment_only: Determines whether to replace or only comment out a line
     :type comment_only boolean:
@@ -25,5 +25,7 @@ def update_line_file(file_path, str_line_to_update, str_replacement, comment_onl
     :returns: whether the string was replaced in the file or it was commented out
     :rtype: boolean
     '''
+    # ! Important; this updates an entire line! If the substring matches
+    #  the line should be obliterated and replaced with str_replacement
     file_modified = False
     return file_modified
