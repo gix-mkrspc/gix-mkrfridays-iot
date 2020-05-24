481d505f63e45c422dace295461468338cf6cf69
codes <hello@cody.codes>
2020-05-24 13:07:22 -0700

    :construction: Add az_cli function, clean up

--- azure_iot_hub/python-sdk/provision.py
+++ azure_iot_hub/python-sdk/provision.py
@@ -2,11 +2,23 @@
 # is installed automatically with the other libraries.
 from azure.common.client_factory import get_client_from_cli_profile
 from azure.mgmt.resource import ResourceManagementClient
+from azure.cli.core import get_default_cli
 
 import os
 import random
 import subprocess
 import json
+
+def az_cli(command):
+    args = command.split()
+    print(args)
+    cli = get_default_cli()
+    cli.invoke(args)
+    if cli.result.result:
+        return cli.result.result
+    elif cli.result.error:
+        raise cli.result.error
+    return True
 
 RESOURCE_GROUP_NAME = "MKRPSC_iot-porg"
 RESOURCE_GROUP_LOCATION = "West US"
@@ -22,12 +34,14 @@ CREATE_IOT_HUB = False
 # idempotency/consistent runs
 #IOT_HUB_NAME = f"{RESOURCE_GROUP_NAME}-{random.randint(1,100000):05}"
 
+IOT_HUB_NAME = "internet-of-porg"
+
 # TODO: wrap this in a try and also use WITH
 IOT_database= open("IoT_device_name.txt", "r")
 IOT_database_list=IOT_database.read()
-# TODO: should be IOT_DEVICE_NAMES
-IOT_HUB_NAME = "internet-of-porg"
-IOT_DEVICE_NAMES=IOT_database_list.split('\n' )     #This will be a list of string
+# TODO: may require rstrip() after split to work on windows + macOS + linux
+# see https://bit.ly/2ytd1NN since there may be a line feed as well 
+IOT_DEVICE_NAMES=IOT_database_list.split('\n')     #This will be a list of string
 
 
 IOT_HUB_SKU = "F1" # free tier
@@ -48,92 +62,68 @@ rg_result = resource_client.resource_groups.create_or_update(
     }
 )
 
-# Within the ResourceManagementClient is an object named resource_groups,
-# which is of class ResourceGroupsOperations, which contains methods like
-# create_or_update.
-#
-# The second parameter to create_or_update here is technically a ResourceGroup
-# object. You can create the object directly using ResourceGroup(location=LOCATION)
-# or you can express the object as inline JSON as shown here. For details,
-# see Inline JSON pattern for object arguments at
-# https://docs.microsoft.com/azure/developer/python/azure-sdk-overview#inline-json-pattern-for-object-arguments.
-
 print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} region")
 
 # install extension if not already installed
-
 print('Checking az cli iot-hub extension...')
+# TODO: az_cli function
 os.system('az extension add --name azure-iot')
 if CREATE_IOT_HUB:
     print('Creating iot hub')
+    # TODO: convert to az cli
     direct_output = subprocess.check_output(['az', 'iot', 'hub', 'create', '--name', IOT_HUB_NAME, \
                                             '--resource-group', RESOURCE_GROUP_NAME, '--sku', IOT_HUB_SKU, '--verbose',\
                                             '--partition-count', IOT_HUB_PARTITION_COUNT])
+    output_clean = direct_output.decode('utf8').replace("\n", '')
+    iot_hub_output = json.loads(output_clean)
 
+# I think ithis is fine since it has all the info needed!
+with open(f'{IOT_HUB_NAME}.json', 'w') as json_file:
+    json.dump(iot_hub_output, json_file)
 
-output_clean = direct_output.decode('utf8').replace("\n", '')
-iotHub_output = json.loads(output_clean)
-# TODO: use at all?
-with open(f'{IOT_HUB_DEVICES}.json', 'w') as json_file:
-    json.dump(iotHub_output, json_file)
-
-# first line: iothub name and connection url internet-of-porg.azurewebsites.***
+# TODO: create a CSV with the following:
 # Device ID, Connection String
 
-# print(iotHub_output["id"])
-# iotHub_output is a parsed json 
+# print(iot_hub_output["id"])
+# iot_hub_output is a parsed json 
 # data can be extracted for function app
-
-
-# The return value is another ResourceGroup object with all the details of the
-# new group. In this case the call is synchronous: the resource group has been
-# provisioned by the time the call returns.
-
-# Optional line to delete the resource group
-#resource_client.resource_groups.delete("PythonSDKExample-ResourceGroup-rg")
-
-#repeated?????????????????????
-
-#os.system(f"az iot hub create --name {IOT_HUB_NAME} "
-#        f"--resource-group {RESOURCE_GROUP_NAME} --sku {IOT_HUB_SKU} --verbose "
-#        f"--partition-count {IOT_HUB_PARTITION_COUNT}"
-#        )
-
-#repeated?????????????????????
-
 
 # TODO: have an option for whether the user is providing a file with strings on each line
 # with the identifiers for the devices or if they just have a finite number defined with a prefix
-for j in range(IOT_HUB_NUM_DEVICES):
-    device_name = IOT_HUB_NAME[j]
-    os.system(f"az iot hub device-identity create -n {IOT_HUB_NAME} "
-            f"-d {device_name}"
-            )
-
-# TODO: use subprocess to parse json and give us a map of the deviceids and connections strings 
-# direct_output = subprocess.check_output('ls', shell=True) #could be anything here.
+# for i in range(IOT_HUB_NUM_DEVICES):
+#     device_name = IOT_HUB_NAME[i]
+#     # TODO: use az_cli function REMEMBER TO REMOVE AZ from command though!
+#     os.system(f"az iot hub device-identity create -n {IOT_HUB_NAME} "
+#             f"-d {device_name}"
+#             )
 
 # TODO: if there's a prefix just append the number i to the end of it
+#  OR if it's from the file just use that name
 for i in range(IOT_HUB_NUM_DEVICES):
-    device_name = f"{IOT_HUB_DEVICE_PREFIX}-{random.randint(1,100000):05}"
-    direct_output = subprocess.check_output(['az', 'iot', 'hub', 'device-identity', 'create', '-n', \
-                                        IOT_HUB_NAME, '-d', device_name])
+    if IOT_DEVICE_NAMES:
+        # TODO: this probably needs to be redone
+        device_name = IOT_DEVICE_NAMES[i]
+    else:
+        device_name = f"{IOT_HUB_DEVICE_PREFIX}-{i}"
+    az_cli(f"iot hub device-identity create -n {IOT_HUB_NAME} -d {device_name}")
+    # direct_output = subprocess.check_output(['az', 'iot', 'hub', 'device-identity', 'create', '-n', \
+    #                                     IOT_HUB_NAME, '-d', device_name])
     output_clean = direct_output.decode('utf8').replace("\n", '')
-    device_output = json.loads(output_clean)                                    
+    device_output = json.loads(output_clean)      
+    # TODO: get the right device parameters here using      
+    #     az_cli(f"iot hub device-identity show-connection-string -d {device_name} -n {IOT_HUB_NAME}")                         
     with open(f'{device_name}.json', 'w') as json_file:
         json.dump(device_output, json_file)                                    
     #output_clean = direct_output.decode('utf8').replace("\n", '')
     #device_output = json.loads(output_clean)
     #output from each device 
 
-from azure.cli.core import get_default_cli
 connec_string=[]
 
-for j in range(IOT_HUB_NUM_DEVICES):
-    device_name=str(IOT_HUB_NAME[j])
-    connec_string[j]= get_default_cli().invoke(['iot', 'hub', 'show-connection-string', '--name',device_name])
-
-
+for i in range(IOT_HUB_NUM_DEVICES):
+    device_name=str(IOT_DEVICE_NAMES[i])
+    az_cli(f"iot hub device-identity show-connection-string -d {device_name} -n {IOT_HUB_NAME}")
+    # connec_string[j]= az_cli("az iot hub device-identity show-connection-string")
 
 # az extension add --name azure-iot
 # az iot hub monitor-events --hub-name {IOT_HUB_NAME}
