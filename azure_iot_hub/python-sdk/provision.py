09ba85640c4a178d0ecd55472469c93cf86fdbb6
holc0831 <32761342+holc0831@users.noreply.github.com>
2020-05-24 10:44:19 -0700

    updated_justin_052420_1044

--- azure_iot_hub/python-sdk/provision.py
+++ azure_iot_hub/python-sdk/provision.py
@@ -8,19 +8,26 @@ import random
 import subprocess
 import json
 
-RESOURCE_GROUP_NAME = "iot-porg"
-RESOURCE_GROUP_LOCATION = "centralus"
+RESOURCE_GROUP_NAME = "MKRPSC_iot-porg"
+RESOURCE_GROUP_LOCATION = "West US"
 
 
 # IOT_HUB_NAME = f"{RESOURCE_GROUP_NAME}-iothub"
 # TODO: This should be grabbing from a text file or other source so that we have
 # idempotency/consistent runs
-IOT_HUB_NAME = f"{RESOURCE_GROUP_NAME}-{random.randint(1,100000):05}"
+#IOT_HUB_NAME = f"{RESOURCE_GROUP_NAME}-{random.randint(1,100000):05}"
+
+IOT_database= open("IoT_device_name.txt", "r")
+IOT_database_list=IOT_database.read()
+IOT_HUB_NAME=IOT_database_list.split('\n' )     #This will be a list of string
+
 
 IOT_HUB_SKU = "F1" # free tier
 IOT_HUB_PARTITION_COUNT = "2" # free tier
 
-IOT_HUB_NUM_DEVICES = 5
+IOT_HUB_NUM_DEVICES = len(IOT_HUB_NAME)  #read the length of the name txt
+
+
 IOT_HUB_DEVICE_PREFIX = "device"
 # Obtain the management object for resources, using the credentials from the CLI login.
 resource_client = get_client_from_cli_profile(ResourceManagementClient)
@@ -50,10 +57,16 @@ print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} 
 print('Checking az cli iot-hub extension...')
 os.system('az extension add --name azure-iot')
 print('Creating iot hub')
-os.system(f"az iot hub create --name {IOT_HUB_NAME} "
-        f"--resource-group {RESOURCE_GROUP_NAME} --sku {IOT_HUB_SKU} --verbose "
-        f"--partition-count {IOT_HUB_PARTITION_COUNT}"
-        )
+
+
+#use a for loop to create n IoT devices in the database
+for i in range (IOT_HUB_NUM_DEVICES)
+    os.system(f"az iot hub create --name {IOT_HUB_NAME[i]} "
+         f"--resource-group {RESOURCE_GROUP_NAME} --sku {IOT_HUB_SKU} --verbose "
+         f"--partition-count {IOT_HUB_PARTITION_COUNT}"
+         )
+
+
 
 # The return value is another ResourceGroup object with all the details of the
 # new group. In this case the call is synchronous: the resource group has been
@@ -62,18 +75,31 @@ os.system(f"az iot hub create --name {IOT_HUB_NAME} "
 # Optional line to delete the resource group
 #resource_client.resource_groups.delete("PythonSDKExample-ResourceGroup-rg")
 
-os.system(f"az iot hub create --name {IOT_HUB_NAME} "
-        f"--resource-group {RESOURCE_GROUP_NAME} --sku {IOT_HUB_SKU} --verbose "
-        f"--partition-count {IOT_HUB_PARTITION_COUNT}"
-        )
+#repeated?????????????????????
+
+#os.system(f"az iot hub create --name {IOT_HUB_NAME} "
+#        f"--resource-group {RESOURCE_GROUP_NAME} --sku {IOT_HUB_SKU} --verbose "
+#        f"--partition-count {IOT_HUB_PARTITION_COUNT}"
+#        )
+
+#repeated?????????????????????
+
 
 # TODO: use subprocess to parse json and give us a map of the deviceids and connections strings 
 # direct_output = subprocess.check_output('ls', shell=True) #could be anything here.
-for i in range(IOT_HUB_NUM_DEVICES):
-    device_name = f"{IOT_HUB_DEVICE_PREFIX}-{random.randint(1,100000):05}"
+for j in range(IOT_HUB_NUM_DEVICES):
+    device_name = IOT_HUB_NAME[j]
     os.system(f"az iot hub device-identity create -n {IOT_HUB_NAME} "
             f"-d {device_name}"
             )
+
+from azure.cli.core import get_default_cli
+connec_string=[]
+
+for j in range(IOT_HUB_NUM_DEVICES):
+    device_name=str(IOT_HUB_NAME[j])
+    connec_string[j]= get_default_cli().invoke(['iot', 'hub', 'show-connection-string', '--name',device_name])
+
 
 
 # az extension add --name azure-iot
