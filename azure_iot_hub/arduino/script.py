42e56e1be76f59cf5a4e3f781131fbe7e90fe1c7
codes <hello@cody.codes>
2020-06-11 18:40:48 -0700

    :bug: Add custom comment, fix unnecessary new lines

--- azure_iot_hub/arduino/script.py
+++ azure_iot_hub/arduino/script.py
@@ -7,7 +7,7 @@ from shutil import copyfile
 ESP8266_PACKAGE_PATH = Path("packages/esp8266/hardware/esp8266/")
 
 # TODO: this is a work in progress and needs to be implemented!
-def update_line_file(file_path, str_line_to_update, str_replacement, comment_only):
+def update_line_file(file_path, str_line_to_update, str_replacement, comment_only=False, comment_str=None):
     '''
     Updates a line on a file with a replacement line or comments it out
 
@@ -19,6 +19,8 @@ def update_line_file(file_path, str_line_to_update, str_replacement, comment_onl
     :type str_replacement str:
     :param comment_only: Determines whether to replace or only comment out a line
     :type comment_only boolean:
+    :param comment_str: Str to use for a comment if commenting
+    :type comment_only str:
     :raises: :class:`FileNotFound`: File couldn't be opened
 
     :returns: whether the string was replaced in the file or it was commented out
@@ -26,13 +28,14 @@ def update_line_file(file_path, str_line_to_update, str_replacement, comment_onl
     '''
     file_modified = False
     for line in fileinput.input(file_path, inplace=True):
+        # TODO: determine if file already modified before changing file_modified
         if line.startswith(str_line_to_update):
+            file_modified = True
             if comment_only:
-                line = "// " + line
+                line = f"{comment_str} {line}"
             else:
                 line = str_replacement
-            file_modified = True
-        print (line)
+        sys.stdout.write(line)
         
     # ! Important; this updates an entire line! If the substring matches
     #  the line should be obliterated and replaced with str_replacement
@@ -41,32 +44,23 @@ def update_line_file(file_path, str_line_to_update, str_replacement, comment_onl
 
 
 def main():
-    # NOTE: You should use forward slashes with pathlib functions.
-    # The Path() object will convert forward slashes into the correct k
-    # ind of slash for the current operating system. Nice!
-
-    # If you want to add on to the path, you can use the / operator directly in your code.
-    # Say goodbye to typing out os.path.join(a, b) over and over.
-    # See: https://bit.ly/3gfS7D8 for more info
     if sys.platform == "darwin":
         ARDUINO_PACKAGES_PATH = Path(Path.home() / "Library/Arduino15")
     elif sys.platform == "linux":
         # TODO: add path here!
         ARDUINO_PACKAGES_PATH = Path(Path.home() / ".arduino15/packages/")
-        print("it's linux i broke it")
     elif sys.platform == "win32":
         # TODO: add path here!
         ARDUINO_PACKAGES_PATH = Path(Path.home() / "AppData/Local/Arduino15")
-        print("it's Windows")
 
     # check if board path is set
     try:
-        print(f"Your Arduino board path for platform {sys.platform} is: {str(ARDUINO_PACKAGES_PATH)}")
+        print(f"Arduino board path for platform {sys.platform} is: {str(ARDUINO_PACKAGES_PATH)}")
     except NameError:
         print(
             f"Error: no valid board path condition for platform: {sys.platform}")
 
-    # Get to file to comment out #define round(x) in the Arduino.h file
+    # Check for and change other versions if they exist
     versions = []
     with os.scandir(str(ARDUINO_PACKAGES_PATH / ESP8266_PACKAGE_PATH)) as entries:
         for version in entries:
@@ -77,19 +71,19 @@ def main():
     for path in versions:
         arduino_header_file = Path(path / "cores/esp8266/Arduino.h")
         if arduino_header_file.exists():
+            print(f"Updating: {str(arduino_header_file)}")
+            # TODO: add logic to detect if backup file exists then skip
             copyfile(arduino_header_file, str(Path(path / "cores/esp8266/Arduino.h.orig")))
-            print(f"Updating {str(arduino_header_file)}")
-            get_update = update_line_file(str(arduino_header_file), "#define round(x)", str_replacement = None, comment_only = True)
-            print(f"Updated {arduino_header_file}: {get_update}")
+            print(f"Backup created: {str(Path(path / 'cores/esp8266/Arduino.h.orig'))}")
+            get_update = update_line_file(str(arduino_header_file), "#define round(x)", str_replacement = None, comment_only = True, comment_str="//")
+            print(f"Updated: {get_update} for  {arduino_header_file}")
         platform_txt_file = Path(path / "platform.txt")
         if platform_txt_file.exists():
+            print(f"Updating: {str(platform_txt_file)}")
             copyfile(platform_txt_file, str(Path(path / "platform.txt.orig")))
-            print(f"Updating {str(platform_txt_file)}")
-            get_update = update_line_file(str(platform_txt_file), "build.extra_flags=", str_replacement = "build.extra_flags=-DESP8266 -DDONT_USE_UPLOADTOBLOB -DUSE_BALTIMORE_CERT", comment_only = False)
-            print(f"Updated {platform_txt_file}: {get_update}")
-    
-
-    # TODO: update platform.txt with changes similar to above but replace line instead of comment
+            print(f"Backup created: {str(Path(path / 'platform.txt.orig'))}")
+            get_update = update_line_file(str(platform_txt_file), "build.extra_flags=", str_replacement = "build.extra_flags=-DESP8266 -DDONT_USE_UPLOADTOBLOB -DUSE_BALTIMORE_CERT")
+            print(f"Updated: {get_update} for  {platform_txt_file}")
 
 main()
 
