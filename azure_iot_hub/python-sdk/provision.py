4d6ce5a8ed199bae41b78a0ee90ff31bb4d1c437
codes <hello@cody.codes>
2020-06-12 13:40:16 -0700

    :pencil: Better docs for parameters

--- azure_iot_hub/python-sdk/provision.py
+++ azure_iot_hub/python-sdk/provision.py
@@ -1,5 +1,3 @@
-# Import the needed management objects from the libraries. The azure.common library
-# is installed automatically with the other libraries.
 from azure.common.client_factory import get_client_from_cli_profile
 from azure.mgmt.resource import ResourceManagementClient
 from azure.cli.core import get_default_cli
@@ -20,21 +18,34 @@ def az_cli(command):
         raise cli.result.error
     return True
 
+# What does this script do?
+# The main purpose of this script is to provision IoT Hub resources on Azure
+
+# Setting CREATE_IOT_HUB to True/False will either create an IOT HUB or not.
+# If you set it to false it will use the IOT_HUB_NAME variable to assume that the hub exists
+CREATE_IOT_HUB = False
+
+# The RESOURCE_GROUP_NAME/RESOURCE_GROUP_LOCATION are where your resources will be created
+# and referenced
 RESOURCE_GROUP_NAME = "MKRPSC_iot-porg"
 RESOURCE_GROUP_LOCATION = "West US"
 
-
-# TODO: add options for determining which resources to create
-# CREATE_IOT_HUB = os.environ(['CREATE_IOT_HUB'])
-CREATE_IOT_HUB = False
-
-
-# IOT_HUB_NAME = f"{RESOURCE_GROUP_NAME}-iothub"
-# TODO: This should be grabbing from a text file or other source so that we have
-# idempotency/consistent runs
-#IOT_HUB_NAME = f"{RESOURCE_GROUP_NAME}-{random.randint(1,100000):05}"
+IOT_HUB_NAME = f"{RESOURCE_GROUP_NAME}-iothub"
+# # TODO: This should be grabbing from a text file or other source so that we have
+# # idempotency/consistent runs
+# IOT_HUB_NAME = f"{RESOURCE_GROUP_NAME}-{random.randint(1,100000):05}"
 
 IOT_HUB_NAME = "internet-of-porg"
+
+# If you have a list of device identifiers, you can pass these in as a file
+#   in the following format:
+#   device_id1
+#   device_id2
+#   ...etc
+# TODO: update file name
+# Otherwise, devices will be created with random identifiers. All identifiers
+# and connection strings will be stored in a file called INSERT FILE NAME HERE.txt
+USE_RANDOM_IDENTIFIERS = False
 
 # TODO: wrap this in a try and also use WITH
 # IOT_database= open("IoT_device_name.txt", "r")
@@ -42,24 +53,24 @@ IOT_HUB_NAME = "internet-of-porg"
 # # TODO: may require rstrip() after split to work on windows + macOS + linux
 # # see https://bit.ly/2ytd1NN since there may be a line feed as well 
 
-# format of file should be:
-# name, connection-string,
-# name2, connectionstring2,
-# 'joey,1234,' -> 
-# {'joey':'1234',
-#  'justin':'12345',}
 # IOT_DEVICE_NAMES=IOT_database_list.split('\n')     #This will be a list of string
 
 # Used to name the devices upon provision
 IOT_DEVICE_NAMES= ['joey','justin','codes']
 
-IOT_HUB_SKU = "F1" # free tier
-IOT_HUB_PARTITION_COUNT = "2" # free tier
 
-# IOT_HUB_NUM_DEVICES = len(IOT_HUB_NAME)  #read the length of the name txt
-IOT_HUB_NUM_DEVICES = 0  #read the length of the name txt
+# The number of devices you want to create.
+# Only applies if you set USE_RANDOM_IDENTIFIERS to True
+# Otherwise it will be the length of IOT_DEVICE_NAMES
+IOT_HUB_NUM_DEVICES = 0 
 
+# The SKU; by default it's set for free tier
+IOT_HUB_SKU = "F1"
+IOT_HUB_PARTITION_COUNT = "2"
+
+# Only applies if you set USE_RANDOM_IDENTIFIERS to True
 IOT_HUB_DEVICE_PREFIX = "device"
+
 # Obtain the management object for resources, using the credentials from the CLI login.
 resource_client = get_client_from_cli_profile(ResourceManagementClient)
 
@@ -71,7 +82,7 @@ rg_result = resource_client.resource_groups.create_or_update(
     }
 )
 
-print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} region")
+print(f"Provisioned/updated resource group {rg_result.name} in the {rg_result.location} region")
 
 # install extension if not already installed
 print('Checking az cli iot-hub extension...')
