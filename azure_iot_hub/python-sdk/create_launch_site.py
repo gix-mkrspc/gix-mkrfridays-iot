b6541eb94f74894cf0ed48100e4f213138487eaa
codes <hello@cody.codes>
2020-06-19 15:42:20 -0700

    :construction: Parameterized card

--- azure_iot_hub/python-sdk/create_launch_site.py
+++ azure_iot_hub/python-sdk/create_launch_site.py
@@ -4,18 +4,28 @@ from yattag import Doc
 doc, tag, text = Doc().tagtext()
 
 TITLE = "GIX IoT Hub"
+PORG_IMG = "https://images-na." \
+           "ssl-images-amazon.com/images/I/917jytDQwJL" \
+           "._AC_SL1500_.jpg"
 
 
-def create_card():
+def create_card(device_name, device_type, url, img):
     with tag('div', klass='col card'):
         with tag('div', klass='row'):
-            with tag('div', klass='row'):
-                with tag('div', klass='col'):
-                    doc.stag(
-                        'img',
-                        src='https://images-na.'
-                        'ssl-images-amazon.com/images/I/917jytDQwJL'
-                        '._AC_SL1500_.jpg')
+            with tag('div', klass='col'):
+                doc.stag('img', src=img)
+            with tag('div', klass='col'):
+                text(f'Name: {device_name}')
+                doc.asis('<br>')
+                text(f'Type: {device_type}')
+        with tag('div', klass='row'):
+            with tag('div', klass='col button'):
+                with tag(
+                        'button',
+                        klass='btn btn-primary',
+                        id='activatebutton',
+                        onclick=f"window.location.href='{url}'"):
+                    text(f"Activate {device_type}")
 
 
 doc.asis('<!DOCTYPE html>')
@@ -28,7 +38,8 @@ with tag('html', lang="en"):
     doc.stag(
         'link',
         rel='stylesheet',
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap"
+             ".min.css",
         crossorigin="anonymous",
         integrity="sha384-"
         "Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm")
@@ -42,12 +53,14 @@ with tag('html', lang="en"):
         with tag('h1'):
             text(TITLE)
         with tag('div', klass='row'):
-            create_card()
+            create_card("Chuck", "Super Porg", "https://example.com", PORG_IMG)
 
 doc.asis('<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>')
 doc.asis('<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>')
 doc.asis('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>')
 print(doc.getvalue())
 
+# TODO: use Path lib
 with open("index.html", 'w') as html_file:
+# with open("./assets/generated_site/index.html", 'w') as html_file:
     html_file.write(doc.getvalue())
